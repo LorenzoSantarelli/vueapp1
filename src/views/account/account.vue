4c998ee896ee9bca9f7adff67fd380375aa4ea8b
Federico Massetti <fmassetti9@gmail.com>
2021-03-12 14:49:54 +0100

    modifica delle tabelle Gestione-account home e CB

--- src/views/account/account.vue
+++ src/views/account/account.vue
@@ -1,17 +1,30 @@
 <!-- Creazione template della pagina con i dati dell'utente -->
 <template>
-    <div class="account-page">
-        <h1>Gestione account di {{username}}</h1>
-        <hr>
-        <div class="user-data">
-            <ul class="left">
-                <li v-for="titoli in title" :key="titoli">{{titoli}}: </li>
-            </ul>
-            <ul class="right">
-                <li v-for="dati in user" :key="dati"> {{dati}}</li>
-            </ul>
-        </div>
-    </div>
+    <div class="container">
+  <h1>Gestione account di {{username}}</h1>
+  <hr>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-">
+  <div class="col"><li>{{title[0]}}: </li></div>
+  <div class="col"><li> {{user[0]}}</li></div>
+  <div class="col"></div>
+  
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-">
+  <div class="col"><li>{{title[1]}}: </li></div>
+  <div class="col"><li> {{user[1]}}</li></div>
+  
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-">
+  <div class="col"><li>{{title[2]}}: </li></div>
+  <div class="col"><li> {{user[2]}}</li></div>
+  
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-">
+  <div class="col"><li>{{title[3]}}: </li></div>
+  <div class="col"><li> {{user[3]}}</li></div>
+  
+</div>
+</div>
 </template>
 
 <!-- Import del file account.js -->
