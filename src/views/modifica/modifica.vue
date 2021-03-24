200fafdc4e3215aba7641f6a9dcd68a58670ddd7
Federico Massetti <fmassetti9@gmail.com>
2021-03-24 16:15:02 +0100

    modifica account, gestione account

--- src/views/modifica/modifica.vue
+++ src/views/modifica/modifica.vue
@@ -5,29 +5,29 @@
     <br>
 
 <div class="mb-3 row">
-    <label for="staticEmail" class="col-sm-1 col-form-label">Email</label>
-    <div class="col-sm-11">
-      <input class="form-control" type="text" aria-label="Disabled input example" value="email" disabled readonly>
+    <label for="inputNome" class="col-sm-2 col-form-label">Nome</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" id="inputNome" v-model="nome">
+    </div>
+    <label for="inputCognome" class="col-sm-2 col-form-label" >Cognome</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" id="inputCognome" v-model="cognome">
     </div>
   </div>
 <div class="mb-3 row">
-    <label for="inputPassword" class="col-sm-1 col-form-label">Nome</label>
-    <div class="col-sm-5">
-      <input type="text" class="form-control" id="inputPassword" v-model="nome">
-    </div>
-    <label for="inputPassword" class="col-sm-1 col-form-label" >Cognome</label>
-    <div class="col-sm-5">
-      <input type="text" class="form-control" id="inputPassword" v-model="cognome">
+    <label for="inputTelefono" class="col-sm-2 col-form-label">Telefono</label>
+    <div class="col-sm-4">
+      <input type="number" class="form-control" id="inputTelefono" v-model="telefono">
     </div>
   </div>
 <div class="mb-3 row">
-    <label for="inputPassword" class="col-sm-1 col-form-label">Telefono</label>
-    <div class="col-sm-5">
-      <input type="number" class="form-control" id="inputPassword" v-model="telefono">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <div class="form-control" type="text" aria-label="Disabled input example"  disabled readonly>{{email}}</div>
     </div>
   </div>
 <div class="row">
-<button class="btn btn-outline-primary btn-details" type="button" style="width: 15%; margin: 25px auto" v-on:click="create()">Salva</button>
+<button class="btn btn-outline-primary btn-details" type="button" style="width: 15%; margin: 25px auto" v-on:click="updateUser()">Salva</button>
 </div>
 </div>
 </template>
