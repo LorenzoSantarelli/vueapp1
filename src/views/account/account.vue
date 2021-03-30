b77998f9ac0b24569151a277fc34eb1a8bd1860f
Federico Massetti <fmassetti9@gmail.com>
2021-03-30 14:52:44 +0200

    modifica layout

--- src/views/account/account.vue
+++ src/views/account/account.vue
@@ -3,7 +3,7 @@
     <div>
 <router-view v-if="$route.name !== 'account'" />
 
-<div v-if="$route.name === 'account'" >
+<div v-if="$route.name === 'account'" style="margin-bottom: 100px" >
   <h1>Gestione account di {{username}}</h1>
   <br>
   <br>
@@ -15,7 +15,58 @@
 </div>
 
 <hr>
-<div class="mb-3 row">
+
+  <div class="row align-items-start">
+    <div class="col-sm-3">
+      <div class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[0]}} :</div>
+    </div>
+    <div class="col-sm-3">
+      <div type="text" class="form-control" style="border: none" aria-label="Nome" aria-describedby="addon-wrapping">{{user[0]}}</div>
+    </div>
+  </div>
+  <div class="row align-items-start">
+    <div class="col-md-3">
+      <div class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[1]}} :</div>
+    </div>
+    <div class="col-sm-3">
+      <div type="text" class="form-control" style="border: none" aria-label="Cognome" aria-describedby="addon-wrapping">{{user[1]}}</div>
+    </div>
+  </div>
+  <div class="row align-items-start">
+    <div class="col-sm-3">
+      <div class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[2]}} :</div>
+    </div>
+    <div class="col-sm-3">
+      <div type="text" class="form-control" style="border: none" aria-label="Cognome" aria-describedby="addon-wrapping">{{user[2]}}</div>
+    </div>
+  </div>
+  <div class="row align-items-start">
+    <div class="col-sm-3">
+      <div class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[3]}} :</div>
+    </div>
+    <div class="col-sm-3">
+      <div type="text" class="form-control" style="border: none" aria-label="Cognome" aria-describedby="addon-wrapping">{{user[3]}}</div>
+    </div>
+  </div>
+
+<!-- <div class="mb-3 input-group flex-nowrap">
+  <span class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[0]}} :</span>
+  <div type="text" class="form-control" style="border: none" aria-label="Nome" aria-describedby="addon-wrapping">{{user[0]}}</div>
+</div>
+<div class="mb-3 input-group flex-nowrap">
+  <span class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[1]}} :</span>
+  <div type="text" class="form-control" style="border: none" aria-label="Cognome" aria-describedby="addon-wrapping">{{user[1]}}</div>
+</div>
+<div class="mb-3 input-group flex-nowrap">
+  <span class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[3]}} :</span>
+  <div type="text" class="form-control" style="border: none" aria-label="Telefono" aria-describedby="addon-wrapping">{{user[3]}}</div>
+</div>
+<div class="mb-3 input-group flex-nowrap">
+  <span class="input-group-text font-weight-bold" style="border: none; background-color: white" id="addon-wrapping">{{title[2]}} :</span>
+  <div type="text" class="form-control" style="border: none" aria-label="Email" aria-describedby="addon-wrapping">{{user[2]}}</div>
+</div> -->
+
+<!-- <div class="mb-3 row">
     <label for="inputNome" class="col-sm-2 col-form-label font-weight-bold">{{title[0]}}</label>
     <div class="col-sm-4">
       <div type="text" class="" id="inputNome" style="margin-top:7px">{{user[0]}}</div>
@@ -36,9 +87,9 @@
     <div class="col-sm-10">
       <div class="" type="text" id="staticEmail" style="margin-top:7px">{{user[2]}}</div>
     </div>
-  </div>
+  </div>-->
 </div>
-    </div>
+    </div> 
 
 </template>
 
